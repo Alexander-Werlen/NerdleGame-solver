50d7c10531b8bbd4fe10d1b9b11d0cd5afff663c
Werlen Alexander <Alexwerlen@gmail.com>
2023-12-19 19:13:46 -0300

    final version

--- nerdleGameSolver.cpp
+++ nerdleGameSolver.cpp
@@ -2,10 +2,15 @@
 #include <fstream>
 #include <cmath>
 #include <string>
+#include <map>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
 using namespace std;
 
 string possibleSolutions8[17723];
+map<string, int> PxAuxiliarMatchingTable; //key=Pattern+Expresion, value: numberOfMatchingExpressions
 
 char simbolosPatronSimilitud[3] = {'0', '1', '2'};
 string patterns[6561];
@@ -28,39 +33,32 @@ void createPatternsRecursionFunction(string prevString, char simbol)
         ::indexOfPattern += 1;
     }
 }
-bool checkPattern(string pattern, string currentExpression, string expression)
-{
+
+string generatePattern(int idxE1, int idxE2){
+    string pattern="";
     for (int i = 0; i < 8; i++)
     {
-        if (pattern[i] == '2' && (currentExpression[i] != expression[i]))
-        {
-            return false;
-        }
-        else if (pattern[i] == '0' && (expression.find(currentExpression[i]) != std::string::npos))
-        {
-            return false;
-        }
-        else if (pattern[i] == '1' && (currentExpression[i] == expression[i] || expression.find(currentExpression[i]) == std::string::npos))
-        {
-            return false;
-        }
+        if(possibleSolutions8[idxE1]==possibleSolutions8[idxE2]) pattern+="2";
+        else if(possibleSolutions8[idxE1].find(possibleSolutions8[idxE2][i]) != std::string::npos) pattern+="1";
+        else pattern+="0";
     }
-
-    return true;
+    return pattern;
 }
-float calculatePx(string pattern, string currentExpression, string possibleSolutions[17723])
-{
-    int Nmatches = 0.0f;
 
-    for (int i = 0; i < 17723; i++)
-    {
-        if (checkPattern(pattern, currentExpression, possibleSolutions[i]))
-        {
-            Nmatches++;
+void preComputePxAuxiliarTable() {
+    for(int idxExpresionFija = 0; idxExpresionFija<17723; idxExpresionFija++){
+        if(idxExpresionFija%500==0)cout<<idxExpresionFija<<"/17723"<<endl;
+        for(int idxExpresionMovil = 0; idxExpresionMovil<17723; idxExpresionMovil++){
+            string pattern = generatePattern(idxExpresionFija, idxExpresionMovil);
+            PxAuxiliarMatchingTable[pattern+possibleSolutions8[idxExpresionFija]]++;
         }
     }
+}
 
-    return (float)Nmatches / 17723;
+float calculatePx(string pattern, string currentExpression)
+{
+    if(PxAuxiliarMatchingTable.find(pattern+currentExpression)!=PxAuxiliarMatchingTable.end()) return (float)PxAuxiliarMatchingTable[pattern+currentExpression] / 17723;
+    else return 0;
 }
 
 float calculateInformationVariables(string expression, string possibleSolutions[17723], string patterns[6561])
@@ -77,7 +75,7 @@ float calculateInformationVariables(string expression, string possibleSolutions[
     for (int i = 0; i < 6561; i++)
     {
         //cout << patterns[i] << "  Einfo: " << EinfoOfExpression << endl;
-        float Px = calculatePx(patterns[i], expression, possibleSolutions);
+        float Px = calculatePx(patterns[i], expression);
 
         if (Px != 0.0f)
         {
@@ -86,6 +84,10 @@ float calculateInformationVariables(string expression, string possibleSolutions[
     }
 
     return EinfoOfExpression;
+}
+
+bool customSortFunc(string info1[2], string info2[2]){
+    return true;
 }
 
 int main()
@@ -101,6 +103,7 @@ int main()
         possibleSolutions8[indexOfSolutionReading] = solutionsText;
         indexOfSolutionReading++;
     }
+    MyReadFile.close();
 
     // Generating array of possible patterns
     for (int i = 0; i < 3; i++)
@@ -108,20 +111,33 @@ int main()
         createPatternsRecursionFunction("", simbolosPatronSimilitud[i]);
     }
 
-    // Calculating information given every equation
+    cout<<"STARTED precomputing PxAuxiliarTable"<<endl; //LOGGING
+    preComputePxAuxiliarTable();
+    cout<<"FINISHED precomputing PxAuxiliarTable"<<endl;
 
-    string informationList[17723][2];
+    cout<<"STARTED computing Einfo of expressions"<<endl;
+    // Calculating information given every equation
+    vector<pair<float, int>> informationList; 
 
     for (int i = 0; i < 17723; i++)
     {
+        if(i%500==0) cout<<i<<"/17723"<<endl; //LOGGING
+
         string expression = possibleSolutions8[i];
-        //cout << expression << endl; //###DEBUG
         float EInfoOfExpression = calculateInformationVariables(expression, possibleSolutions8, patterns);
 
-        informationList[i][0] = expression;
-        informationList[i][1] = to_string(EInfoOfExpression);
-
-        //cout << expression << " Einfo: " << EInfoOfExpression << endl; //#DEBUG
+        informationList.push_back({EInfoOfExpression, i});
     }
+
+    sort(informationList.rbegin(), informationList.rend());
+
+    //saving data in .txt
+    ofstream OutputFile("GuessesEinfo8.txt");
+    for (int i = 0; i < 17723; i++)
+    {
+        OutputFile<<possibleSolutions8[informationList[i].second]<<": "<<informationList[i].first<<endl;
+    }
+    OutputFile.close();
+    
     return 0;
 }
